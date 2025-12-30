ac2ff9a212c42b8e35e76b94c8cd906f786be9c6
iciclejj <55782191+iciclejj@users.noreply.github.com>
2025-12-30 19:14:47 +0100

    Move code into draw_frame()

--- src/event-handlers/surface.c
+++ src/event-handlers/surface.c
@@ -41,6 +41,37 @@ get_free_double_buffer(struct client_state *state)
     return buffer;
 }
 
+static void
+draw_frame(
+    struct client_state *state,
+    struct client_state_surface_buffer *st_buffer
+) {
+    // XXX TEST TODO: Improve draw_frame
+
+    struct client_state_selection_blend2d *bl = &state->selection.bl;
+
+    bl_context_begin(&bl->ctx, &st_buffer->bl_img, NULL);
+    memset(st_buffer->data, 0, state->surface.buf_size);
+
+    bl->rect.x = bl->box.x0;
+    bl->rect.y = bl->box.y0;
+    bl->rect.w = bl->box.x1 - bl->box.x0;
+    bl->rect.h = bl->box.y1 - bl->box.y0;
+    normalize_rect_i(&bl->rect);
+
+    fprintf(
+        stderr, "box: x0=%d, x1=%d, y0=%d, y1=%d\n",
+        bl->box.x0, bl->box.x1, bl->box.y0, bl->box.y1
+    );
+    fprintf(
+        stderr, "Rect: x=%d, y=%d, w=%d, h=%d\n",
+        bl->rect.x, bl->rect.y, bl->rect.w, bl->rect.h
+    );
+
+    bl_context_fill_rect_i_rgba32(&bl->ctx, &bl->rect, 0x88888888);
+    bl_context_end(&bl->ctx);
+}
+
 void
 surface_frame_callback_handler(
     void *data,
@@ -77,30 +108,9 @@ surface_frame_callback_handler(
 
     st_buffer->busy = true;
 
-    struct client_state_selection_blend2d *bl = &state->selection.bl;
-
-    // XXX TEST TODO: Improve this
-    bl_context_begin(&bl->ctx, &st_buffer->bl_img, NULL);
-    memset(st_buffer->data, 0, state->surface.buf_size);
-
-    // XXX TEST
-    state->selection.bl.rect.x = bl->box.x0;
-    state->selection.bl.rect.y = bl->box.y0;
-    bl->rect.w = bl->box.x1 - bl->box.x0;
-    bl->rect.h = bl->box.y1 - bl->box.y0;
-    normalize_rect_i(&bl->rect);
-    fprintf(
-        stderr, "box: x0=%d, x1=%d, y0=%d, y1=%d\n",
-        bl->box.x0, bl->box.x1, bl->box.y0, bl->box.y1
-    );
-    fprintf(
-        stderr, "Rect: x=%d, y=%d, w=%d, h=%d\n",
-        bl->rect.x, bl->rect.y, bl->rect.w, bl->rect.h
-    );
-    bl_context_fill_rect_i_rgba32(&bl->ctx, &bl->rect, 0x88888888);
-    bl_context_end(&bl->ctx);
-
+    draw_frame(state, st_buffer);
     wl_surface_attach(state->surface.surface, st_buffer->buffer, 0, 0);
+
     // TODO: Calculate damage area to not re-draw entire surface every frame
     wl_surface_damage_buffer(
         state->surface.surface,
@@ -110,9 +120,6 @@ surface_frame_callback_handler(
         state->surface.height
     );
     wl_surface_commit(state->surface.surface);
-
-    // fprintf(stderr, "Committed buffer %p\n", st_buffer->buffer);
-    // fprintf(stderr, "Color: %x\n", state->surface.curr_color);
 }
 
 struct wl_callback_listener surface_frame_callback_listener = {
