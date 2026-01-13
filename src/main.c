7d455f23bdb804b380b01528f15de2a062cbda67
iciclejj <55782191+iciclejj@users.noreply.github.com>
2026-01-13 17:56:05 +0100

    Move main() code into functions

--- src/main.c
+++ src/main.c
@@ -126,45 +126,49 @@ init_selection_and_blend2d(struct client_state_output *st_output)
     return true;
 }
 
-// TODO: Allow selection before capture protocols are ready?
-//           Probably negligible and difficult without multithreading
-//       Probably find a cleaner way to do this multi-step init?
-//       Remember to fix off-by-one bug when selecting corner to corner
-//           F.ex. 2559x1599 rect width/height
-int main(void)
+// TODO: void return type?
+static bool
+init_premem(struct client_state *state)
 {
-    struct client_state state = { };
-
-    if (!init_wayland_globals_and_roundtrip(&state)) {
-        return EXIT_FAILURE;
+    // TODO: Unwrap this ?
+    if (!init_wayland_globals_and_roundtrip(state)) {
+        return false;
     }
 
     //   Collect dynamic memory requirements
     // + Initialize otherwhat lacking extra dependencies (beyond globals)
-    for (int i = 0; i < state.n_outputs; ++i) {
-        struct client_state_output *_st_output = &state.outputs[i];
+    for (int i = 0; i < state->n_outputs; ++i) {
+        struct client_state_output *_st_output = &state->outputs[i];
 
-        if (!init_output_surface(_st_output, &state.globals)) {
-            return EXIT_FAILURE;
+        if (!init_output_surface(_st_output, &state->globals)) {
+            return false;
         }
 
-        if (!init_capture(_st_output, &state.globals)) {
-            return EXIT_FAILURE;
+        if (!init_capture(_st_output, &state->globals)) {
+            return false;
         }
     }
     // Then roundtrip to collect listener-provided memory requirements into state
     // This should be our last required roundtrip until main event loop's dispatch
-    wl_display_roundtrip(state.globals.display);
+    wl_display_roundtrip(state->globals.display);
 
+    return true;
+}
+
+static bool
+init_meminit(
+    struct client_state *state,
+    size_t *global_pool_size_bytes,
+    void **global_pool
+) {
     // Calculate memory requirements
-    int global_pool_size_bytes = 0;
-    for (int i = 0; i < state.n_outputs; ++i) {
-        struct client_state_output *_st_output = &state.outputs[i];
+    for (int i = 0; i < state->n_outputs; ++i) {
+        struct client_state_output *_st_output = &state->outputs[i];
 
         // XXX: Handle this gracefully (and maybe in a nicer location?)
         if (_st_output->capture.shm_format == -1) {
             DEBUG("Failed to select shm_buffer format.\n");
-            return EXIT_FAILURE;
+            return false;
         }
 
         const ssize_t _surface_buf_bytes = SURFACE_BUF_COUNT * GET_SURFACE_BUF_SIZE(_st_output->mode);
@@ -172,34 +176,34 @@ int main(void)
         const ssize_t _capture_buf_iov_bytes = GET_CAPTURE_IOV_SIZE((*_st_output));
         // selection: No manual allocations
 
-        global_pool_size_bytes += _surface_buf_bytes
+        *global_pool_size_bytes += _surface_buf_bytes
                                 + _capture_buf_bytes
                                 + _capture_buf_iov_bytes;
     }
 
     int global_pool_shm_fd = shm_open_anon();
-    if (ftruncate(global_pool_shm_fd, global_pool_size_bytes) == -1) {
-        DEBUG("Failed to resize shm file to %d\n", global_pool_size_bytes);
+    if (ftruncate(global_pool_shm_fd, *global_pool_size_bytes) == -1) {
+        DEBUG("Failed to resize shm file to %zu\n", *global_pool_size_bytes);
         close(global_pool_shm_fd);
-        return EXIT_FAILURE;
+        return false;
     }
 
-    void *global_pool = mmap(NULL, global_pool_size_bytes, PROT_READ | PROT_WRITE, MAP_SHARED, global_pool_shm_fd, 0);
+    *global_pool = mmap(NULL, *global_pool_size_bytes, PROT_READ | PROT_WRITE, MAP_SHARED, global_pool_shm_fd, 0);
     struct wl_shm_pool *global_pool_wl = wl_shm_create_pool(
-        state.globals.shm,
+        state->globals.shm,
         global_pool_shm_fd,
-        global_pool_size_bytes
+        *global_pool_size_bytes
     );
 
     // Assign allocated memory
     ssize_t curr_offset = 0;
-    for (int i = 0; i < state.n_outputs; ++i) {
-        struct client_state_output *_st_output = &state.outputs[i];
+    for (int i = 0; i < state->n_outputs; ++i) {
+        struct client_state_output *_st_output = &state->outputs[i];
 
         assert(SURFACE_BUF_COUNT == A_DOUBLE_BUFFER_HAS_TWO_BUFFERS && SURFACE_BUF_COUNT == 2);
 
         for (int i = 0; i < SURFACE_BUF_COUNT; i++) {
-            _st_output->surface.double_buffer[i].data = global_pool + curr_offset;
+            _st_output->surface.double_buffer[i].data = *global_pool + curr_offset;
             _st_output->surface.double_buffer[i].buffer = wl_shm_pool_create_buffer(
                 global_pool_wl,
                 curr_offset,
@@ -220,7 +224,7 @@ int main(void)
             wl_surface_attach(_st_output->surface.surface, _st_output->surface.double_buffer[0].buffer, 0, 0);
         }
 
-        _st_output->capture.buffer.data = global_pool + curr_offset;
+        _st_output->capture.buffer.data = *global_pool + curr_offset;
         _st_output->capture.buffer.buffer = wl_shm_pool_create_buffer(
             global_pool_wl,
             curr_offset,
@@ -237,20 +241,26 @@ int main(void)
             &_st_output->capture.buffer
         );
 
-        _st_output->capture.frame_iovec =  global_pool + curr_offset;
+        _st_output->capture.frame_iovec =  *global_pool + curr_offset;
     }
 
     // (wayland's mmaps and fd references live on)
     wl_shm_pool_destroy(global_pool_wl);
     close(global_pool_shm_fd);
 
+    return true;
+}
+
+static bool
+init_postmem(struct client_state *state)
+{
     // Memory init finished - run remaining dependent initialization
-    assert(state.n_outputs <= MAX_OUTPUTS);
-    for (int i = 0; i < state.n_outputs; ++i) {
-        struct client_state_output *_st_output = &state.outputs[i];
+    assert(state->n_outputs <= MAX_OUTPUTS);
+    for (int i = 0; i < state->n_outputs; ++i) {
+        struct client_state_output *_st_output = &state->outputs[i];
 
         if (!init_selection_and_blend2d(_st_output)) {
-            return EXIT_FAILURE;
+            return false;
         }
 
         // Initial frame callback request.
@@ -270,6 +280,32 @@ int main(void)
         //           relatively fast (Ex: ~130us)
         wl_surface_commit(_st_output->surface.surface);
     }
+
+    return true;
+}
+
+// TODO: Allow selection before capture protocols are ready?
+//           Probably negligible and difficult without multithreading
+//       Probably find a cleaner way to do this multi-step init?
+//       Remember to fix off-by-one bug when selecting corner to corner
+//           F.ex. 2559x1599 rect width/height
+int main(void)
+{
+    struct client_state state = { };
+    size_t global_pool_size_bytes = 0;
+    void *global_pool = NULL;
+
+    if (!init_premem(&state)) {
+        eprintf("Failed pre-memory allocation initialization.\n");
+        return EXIT_FAILURE;
+    }
+
+    if (!init_meminit(&state, &global_pool_size_bytes, &global_pool)) {
+        eprintf("Failed to initialize memory and/or shared memory buffers.\n");
+        return EXIT_FAILURE;
+    }
+
+    init_postmem(&state);
 
     while ( // Main event loop...
         !state.exit_requested
