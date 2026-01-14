274a894e10350cdec6b77848889f50c4fc009b87
iciclejj <55782191+iciclejj@users.noreply.github.com>
2026-01-14 15:27:55 +0100

    Rename row_bytes -> area_row_bytes

--- src/event-handlers/image_copy_capture_frame.c
+++ src/event-handlers/image_copy_capture_frame.c
@@ -67,7 +67,7 @@ handle_image_copy_capture_frame_ready(
 
     uint32_t area_width_px   = frame_ctx->capture_area_px.x1 - frame_ctx->capture_area_px.x0;
     uint32_t area_height_px  = frame_ctx->capture_area_px.y1 - frame_ctx->capture_area_px.y0;
-    uint32_t row_bytes       = frame_ctx->pixel_stride * area_width_px;
+    uint32_t area_row_bytes       = frame_ctx->pixel_stride * area_width_px;
 
     char *addr =
         frame_ctx->st_buffer.data
@@ -78,7 +78,7 @@ handle_image_copy_capture_frame_ready(
     // assert(GET_CAPTURE_IOV_LEN((*st_output)) >= height_px);
     for (int i = 0; i < area_height_px; ++i) {
         frame_ctx->frame_iovec[i].iov_base = addr;
-        frame_ctx->frame_iovec[i].iov_len = row_bytes;
+        frame_ctx->frame_iovec[i].iov_len = area_row_bytes;
         addr += frame_ctx->pixel_stride * frame_ctx->source_width_px;
     }
 
