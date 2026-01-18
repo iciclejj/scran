7dbdfb51003f41aa49aa2d5cf62e037cc1733b73
iciclejj <55782191+iciclejj@users.noreply.github.com>
2026-01-18 15:55:23 +0100

    nix: Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,3 @@
 { pkgs, ... }:
 
-# let
-#   pkg = pkgs.callPackage ./default.nix { };
-# in
-(pkgs.callPackage ./default.nix { }).overrideAttrs {
-  hardeningDisable = [ "all" ];
-}
+pkgs.callPackage ./default.nix { _target = "debug"; }
