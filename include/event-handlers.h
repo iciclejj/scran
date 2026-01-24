cb46b6b6a6021688472239e4f1354eb43e34bdb3
iciclejj <55782191+iciclejj@users.noreply.github.com>
2026-01-25 00:52:00 +0100

    Remove now-unused noop function

--- include/event-handlers.h
+++ include/event-handlers.h
@@ -9,11 +9,6 @@
 //   * Find out how to check whether a given event handler is required.
 //   * Go through every listener and make sure we have all desired events handled
 
-static void
-noop(/* XXX: leave blank to swallow args. TODO: Remove this... */)
-{
-};
-
 extern struct wl_pointer_listener pointer_listener;
 extern struct zwlr_layer_surface_v1_listener layer_surface_listener;
 extern struct wl_buffer_listener surface_buffer_listener;
