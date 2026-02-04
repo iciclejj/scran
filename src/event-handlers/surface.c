310c8e4c9dd7a4d9db0b12ae05013491969f78f0
iciclejj <55782191+iciclejj@users.noreply.github.com>
2026-02-05 00:24:06 +0100

    Fix clear/clip order in surface::frame

--- src/event-handlers/surface.c
+++ src/event-handlers/surface.c
@@ -130,8 +130,8 @@ draw_frame_and_damage_buffer(
     // just make _box_diffs an array that we can loop through.
 
     damage_region = blboxi_to_blrecti(box_diffs.left_full);
-    bl_context_clear_all(&st_buffer->bl_ctx);
     bl_context_clip_to_rect_i(&st_buffer->bl_ctx, &damage_region);
+    bl_context_clear_all(&st_buffer->bl_ctx);
     bl_context_fill_path_d(&st_buffer->bl_ctx, &origin, &st_output->surface.bl_path);
     bl_context_restore_clipping(&st_buffer->bl_ctx);
     wl_surface_damage_buffer( st_output->surface.wl_surface,
