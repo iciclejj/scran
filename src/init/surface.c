228d77779162ab51155f4223eed2728e9b34a922
iciclejj <55782191+iciclejj@users.noreply.github.com>
2026-02-14 16:01:09 +0100

    Draw only into a single selection frame-buffer during init

--- src/init/surface.c
+++ src/init/surface.c
@@ -52,11 +52,10 @@ dispatch_surface_event_loop(struct scran_output *st_output)
 {
     // TODO: Assert bl_ctx has already begun, or maybe just move its (entire?)
     // init into here.
+    struct BLPathCore *bl_path = &st_output->surface.bl_path;
 
     for (int i = 0; i < SURFACE_BUF_COUNT; ++i) {
         struct scran_output_surface_buffer *st_buffer = &st_output->surface.double_buffer[i];
-        struct scran_output_selectionContext *selection_ctx = &st_output->selection_ctx;
-        struct BLPathCore *bl_path = &st_output->surface.bl_path;
 
         bl_context_set_fill_style_rgba32(&st_buffer->bl_ctx, BLCONTEXT_RGBA32_FILL_STYLE_DEFAULT.value);
         // Even-odd fill rule because we will use two overlapping rects to create
@@ -64,18 +63,17 @@ dispatch_surface_event_loop(struct scran_output *st_output)
         //   NOTE: Just move this back into the ::frame handler if we will need it
         //   for more complicated rendering in this blcontext than just a square...
         bl_context_set_fill_rule(&st_buffer->bl_ctx, BL_FILL_RULE_EVEN_ODD);
-
-        bl_path_add_box_i(bl_path, &selection_ctx->bl_box_bounds, BL_GEOMETRY_DIRECTION_NONE);
-
-        // XXX: At the moment, this function is only used at the start of the
-        // program. Handle busy buffers later if/when it will be necessary.
-        assert(st_buffer->busy == false);
-        bl_context_fill_path_d(&st_buffer->bl_ctx, &SURFACE_BLCONTEXT_ORIGIN, bl_path);
-
-        bl_path_reset(bl_path);
     }
 
     struct scran_output_surface_buffer *const initial_buffer = &st_output->surface.double_buffer[0];
+    struct BLBoxI *selection_box_bounds = &st_output->selection_ctx.bl_box_bounds;
+
+    bl_path_add_box_i(bl_path, selection_box_bounds, BL_GEOMETRY_DIRECTION_NONE);
+    // XXX: At the moment, this function is only used at the start of the
+    // program. Handle busy buffers later if/when it will be necessary.
+    assert(initial_buffer->busy == false);
+    bl_context_fill_path_d(&initial_buffer->bl_ctx, &SURFACE_BLCONTEXT_ORIGIN, bl_path);
+    bl_path_reset(bl_path);
 
     initial_buffer->busy = true;
     wl_surface_attach(st_output->surface.wl_surface, initial_buffer->wl_buffer, 0, 0);
