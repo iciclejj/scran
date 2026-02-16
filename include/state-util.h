36ff446b30a5e775b0a53d4eda6b0273b032905d
iciclejj <55782191+iciclejj@users.noreply.github.com>
2026-02-16 18:23:54 +0100

    Add get_output_array_index()

--- include/state-util.h
+++ include/state-util.h
@@ -1,7 +1,12 @@
 #ifndef STATE_UTIL_H
 #define STATE_UTIL_H
 
+#include <assert.h>
+
 #include "state.h"
+#include "stddef.h"
+
+extern struct scran g_state;
 
 
 static inline int32_t
@@ -18,6 +23,18 @@ get_output_height_logical(struct scran_output *st_output) {
         || st_output->transform == WL_OUTPUT_TRANSFORM_270
          ? st_output->mode.width_px
          : st_output->mode.height_px;
+}
+
+static inline uint8_t
+get_output_array_index(const struct scran_output *st_output) {
+    ptrdiff_t index = st_output - g_state.outputs;
+
+    assert(index >= 0);
+    assert(index < MAX_OUTPUTS);
+    assert(index <= UINT8_MAX);
+    assert(g_state.outputs <= st_output && st_output < (g_state.outputs + MAX_OUTPUTS));
+
+    return (uint8_t)index;
 }
 
 
