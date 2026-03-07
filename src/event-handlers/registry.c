b3fae3ef63176b0381e37f3092002f11854a1f84
iciclejj <55782191+iciclejj@users.noreply.github.com>
2026-03-07 05:14:05 +0100

    Correct required protocol versions in registry::global

--- src/event-handlers/registry.c
+++ src/event-handlers/registry.c
@@ -27,44 +27,47 @@ registry_handle_global(
 
     #define _INTERFACE_IS(desired) (strcmp(interface, desired.name) == 0)
 
-    // TODO(!!!): Determine desired minimum versions.
     if (_INTERFACE_IS(wl_compositor_interface)) {
-        globals->compositor = wl_registry_bind(registry, name, &wl_compositor_interface, version);
+        // v4 => wl_surface v4 => wl_surface::damage_buffer
+        globals->compositor = wl_registry_bind(registry, name, &wl_compositor_interface, 4);
     } else if (_INTERFACE_IS(wl_seat_interface)) {
         if (globals->seat != NULL) {
             // TODO: wl_list of seats
             DEBUG("Ignoring additional wl_seat global.\n");
         } else {
             DEBUG("Adding seat... ");
-            globals->seat    = wl_registry_bind(registry, name, &wl_seat_interface, version);
-            // TODO: Do this elsewhere? De-spaghetti everything later...
+            // For future reference: v2 => ::name, v5 => ::release
+            globals->seat    = wl_registry_bind(registry, name, &wl_seat_interface, 1);
+            // TODO: Do this elsewhere?
             wl_seat_add_listener(globals->seat, &seat_listener, state);
             DEBUG("added seat listener.\n");
         }
     } else if (_INTERFACE_IS(wl_shm_interface)) {
-        globals->shm         = wl_registry_bind(registry, name, &wl_shm_interface, version);
+        globals->shm         = wl_registry_bind(registry, name, &wl_shm_interface, 1);
     } else if (_INTERFACE_IS(zwlr_layer_shell_v1_interface)) {
-        // XXX: version 4 cus nixpkgs
-        globals->layer_shell = wl_registry_bind(registry, name, &zwlr_layer_shell_v1_interface, 4);
+        // v3 => ::destroy
+        globals->layer_shell = wl_registry_bind(registry, name, &zwlr_layer_shell_v1_interface, 3);
     } else if (_INTERFACE_IS(wp_cursor_shape_manager_v1_interface)) {
         // sway only has version 1 at the time of writing.
         globals->cursor_shape_manager = wl_registry_bind(registry, name, &wp_cursor_shape_manager_v1_interface, 1);
     } else if (_INTERFACE_IS(wl_output_interface)) {
+        // TODO: Handle adding/removing outputs during program runtime?
         if (state->n_outputs >= MAX_OUTPUTS) {
             DEBUG("Maximum output limit reached: %d\n", MAX_OUTPUTS);
             return;
         }
-        // TODO: Handle adding/removing outputs during program runtime?
 
         struct scran_output *curr_output = &state->outputs[state->n_outputs];
 
-        curr_output->wl_output = wl_registry_bind(registry, name, &wl_output_interface, 4);
+        // For future reference: v2 => ::done, v4 => ::name & ::description.
+        //     (Not exhaustive)
+        curr_output->wl_output = wl_registry_bind(registry, name, &wl_output_interface, 1);
         wl_output_add_listener(curr_output->wl_output, &output_listener, curr_output);
 
         ++state->n_outputs;
     } else if (_INTERFACE_IS(zxdg_output_manager_v1_interface)) {
-        // TODO: Acutally implement this..?
-        globals->xdg_output_manager = wl_registry_bind(registry, name, &zxdg_output_manager_v1_interface, 3);
+        // XXX TODO: Actually implement this..?
+        globals->xdg_output_manager = wl_registry_bind(registry, name, &zxdg_output_manager_v1_interface, 1);
     } else if (_INTERFACE_IS(ext_output_image_capture_source_manager_v1_interface)) {
         globals->output_image_capture_source_manager = wl_registry_bind(registry, name, &ext_output_image_capture_source_manager_v1_interface, 1);
     } else if (_INTERFACE_IS(ext_image_copy_capture_manager_v1_interface)) {
