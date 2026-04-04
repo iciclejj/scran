e8da945f410949f723dfb65a2e630bb05a67c33e
iciclejj <55782191+iciclejj@users.noreply.github.com>
2026-04-04 18:06:40 +0200

    Add DEBUG_BLRECTI

--- include/print.h
+++ include/print.h
@@ -19,6 +19,7 @@
 #define DEBUG(fmt, ...) _DEBUG("DEBUG: " fmt, ##__VA_ARGS__)
 
 #define DEBUG_BLBOXI(box) DEBUG("x0=%d, y0=%d, x1=%d, y1=%d\n", box.x0, box.y0, box.x1, box.y1)
+#define DEBUG_BLRECTI(rect) DEBUG("x=%d, y=%d, w=%d, h=%d\n", rect.x, rect.y, rect.w, rect.h)
 #define DEBUG_POINT(x, y) DEBUG("x=%d, y=%d\n", (x), (y))
 
 
