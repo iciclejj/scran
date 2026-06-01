324db5fb6b37d5222127893b81fc6d7f3d3a69ec
iciclejj <55782191+iciclejj@users.noreply.github.com>
2026-06-02 01:47:05 +0200

    Unify force-recapture damage logic

--- src/capture.c
+++ src/capture.c
@@ -478,27 +478,13 @@ request_end_video_capture(struct scran_output *st_output)
     // timestamp. This also lets the frame listener finalize the
     // recording and clean up as soon as possible.
     ext_image_copy_capture_frame_v1_destroy(st_output->capture.frame_ctx.frame);
-
-    // XXX TODO: Client-requested damage does *not* necessarily trigger a new
-    // ::ready from the compositor, if no actual damage has occurred. Sway, for
-    // example, ignores it, and forces us to wait for an indefinite amount of
-    // time for the capture to end, if no movement has happened on screen.
-    //
-    // The optimal solution might be to handle this internally anyways, either
-    // manually duplicating it or changing the frame duration retroactively.
-    //
-    // HACK: For now, just force a surface redraw to effectively force the
-    // compositor to send us another capture frame... (May not work if only
-    // the transparent part of the selection surface is visible, e.g. with
-    // "fullscreen" capture, but seems effective even then on my local
-    // Sway (v1.11).)
-    set_force_redraw_selection_surface_buffers(st_output);
-    request_selection_surface_frame_callback(st_output);
-
     request_video_capture_frame(
         &st_output->capture.frame_ctx,
         0, 0, st_output->mode.width_px, st_output->mode.height_px
     );
+
+    force_compositor_output_damage_for_capture(st_output);
+
     st_output->capture.frame_ctx.capturing_video = false;
 }
 
--- src/event-handlers/presentation-feedback/selection-transparent-for-freezeframe.c
+++ src/event-handlers/presentation-feedback/selection-transparent-for-freezeframe.c
@@ -1,6 +1,7 @@
 #include "presentation-time.h"
 
 #include "freezeframe.h"
+#include "capture.h"
 #include "print.h"
 
 static inline void
@@ -25,14 +26,10 @@ handle_presentation_feedback_presented__selection_transparent_for_freezeframe(
 
     request_freezeframe_assume_callback_set(st_output);
 
-    // HACK: Force some output damage, since some compositors (like Hyprland on
-    // rapid consecutive freezeframe refreshes) may wait indefinitely for the
-    // next capture frame if no damage is detected.
-    //
-    // Simply doing an empty commit, without damage, seems enough for Hyprland,
-    // but we'll explicitly damage it just for good measure...
-    wl_surface_damage_buffer(st_output->selection_surface.surface.wl_surface, 0, 0, 1, 1);
-    wl_surface_commit(st_output->selection_surface.surface.wl_surface);
+    // Force some output damage, since some compositors (like Hyprland on rapid
+    // consecutive freezeframe refreshes) may wait indefinitely for the next
+    // capture frame if no damage is detected.
+    force_compositor_output_damage_for_capture(st_output);
 }
 
 static inline void
