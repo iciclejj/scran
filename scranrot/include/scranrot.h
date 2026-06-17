e5f3b07af2665b5145ede2755940d0f525146d80
iciclejj <55782191+iciclejj@users.noreply.github.com>
2026-06-17 15:43:02 +0200

    scranrot: Make include/scranrot.h work with both C and C++
    Assumes C compiler defines __restrict

--- scranrot/include/scranrot.h
+++ scranrot/include/scranrot.h
@@ -4,13 +4,16 @@
 
 /* ~ because scrot was taken ~ */
 
-
 /* Copyright (c) 2026 iciclejj. MIT License, same as scran. */
-
 
 
 #include <stdint.h>
 #include <stdbool.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 
 #define SCRANROT_ALWAYS_INLINE \
@@ -39,11 +42,11 @@ enum scranrot_transform {
 };
 
 typedef bool scranrot_transform_framebuffer_to_yuv_fn(
-    const uint8_t *restrict src,
+    const uint8_t *__restrict src,
     int src_width_px,
     int src_height_px,
     int src_stride_bytes,
-    uint8_t *restrict dst,
+    uint8_t *__restrict dst,
     // Reorders dst's pixel byte-order relative to src.
     //   8-bit-valued mask representing new order
     //     I.e. 0x03000201 => 3, 0, 2, 1 => (RGBA -> ARBG)
@@ -56,11 +59,11 @@ typedef bool scranrot_transform_framebuffer_to_yuv_fn(
 );
 
 typedef bool scranrot_transform_framebuffer_fn(
-    const uint8_t *restrict src,
+    const uint8_t *__restrict src,
     int src_width_px,
     int src_height_px,
     int src_stride_bytes,
-    uint8_t *restrict dst,
+    uint8_t *__restrict dst,
     // Reorders dst's pixel byte-order relative to src.
     //   8-bit-valued mask representing new order
     //     I.e. 0x03000201 => 3, 0, 2, 1 => (RGBA -> ARBG)
@@ -74,11 +77,11 @@ typedef bool scranrot_transform_framebuffer_fn(
 // scranrot_init() must be called before using this function.
 bool
 scranrot_transform_framebuffer(
-    const uint8_t *restrict src,
+    const uint8_t *__restrict src,
     int src_width_px,
     int src_height_px,
     int src_stride_bytes,
-    uint8_t *restrict dst,
+    uint8_t *__restrict dst,
     // Reorder src pixel byte-order before moving to dst
     // 8-bit-valued mask representing new order
     //     I.e. 0x03000201 => 3, 0, 2, 1 => (RGBA -> ARBG)
@@ -91,11 +94,11 @@ scranrot_transform_framebuffer(
 // scranrot_init() must be called before using this function.
 bool
 scranrot_transform_framebuffer_to_yuv420(
-    const uint8_t *restrict src,
+    const uint8_t *__restrict src,
     int src_width_px,
     int src_height_px,
     int src_stride_bytes,
-    uint8_t *restrict dst,
+    uint8_t *__restrict dst,
     // Reorder src pixel byte-order before moving to dst
     // 8-bit-valued mask representing new order
     //     I.e. 0x03000201 => 3, 0, 2, 1 => (RGBA -> ARBG)
@@ -117,5 +120,9 @@ scranrot_transform_framebuffer_fn scranrot_transform_framebuffer_fallback;
 scranrot_transform_framebuffer_to_yuv_fn scranrot_transform_framebuffer_to_yuv420_ssse3__unaligned;
 scranrot_transform_framebuffer_to_yuv_fn scranrot_transform_framebuffer_to_yuv420_fallback;
 
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 #endif
