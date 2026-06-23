68fa304bfe9f327b86b80ed9ef55a8e4c76fa994
iciclejj <55782191+iciclejj@users.noreply.github.com>
2026-06-23 18:45:45 +0200

    scranrot: Cleanup

--- scranrot/src/sse2.hpp
+++ scranrot/src/sse2.hpp
@@ -14,10 +14,13 @@ namespace scranrot::internal {
     static inline __m128i
     scranrot_sse2_rgba_shuffle_to_m128i(u32 rgba_shuffle_mask) {
         // TODO: Assert rgba_shuffle is valid (and let (0 => 0,1,2,3) ?)
-        const __m128i _rgba_shuffle_mask_128_offsets = _mm_setr_epi8(0,0,0,0, 4,4,4,4, 8,8,8,8, 12,12,12,12);
-        const __m128i _rgba_shuffle_mask_128 = _mm_set1_epi32(rgba_shuffle_mask);
-        const __m128i rgba_shuffle_mask_128 = _mm_add_epi8(_rgba_shuffle_mask_128_offsets, _rgba_shuffle_mask_128);
-        return rgba_shuffle_mask_128;
+        const __m128i rgba_shuffle_mask_128_offsets = _mm_setr_epi8(
+            0,0,0,0, 4,4,4,4, 8,8,8,8, 12,12,12,12
+        );
+        return _mm_add_epi8(
+            rgba_shuffle_mask_128_offsets,
+            _mm_set1_epi32(rgba_shuffle_mask)
+        );
     }
 
     SCRANROT_ALWAYS_INLINE
