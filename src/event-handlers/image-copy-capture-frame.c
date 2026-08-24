76e30ecf4d39716eab123765096573eb6d872190
iciclejj <55782191+iciclejj@users.noreply.github.com>
2026-08-24 19:44:16 +0200

    Move freezeframe code out of event-handler
    Event-handler is now stripped of most implementation-details

--- src/event-handlers/image-copy-capture-frame.c
+++ src/event-handlers/image-copy-capture-frame.c
@@ -5,14 +5,8 @@
 
 #include "capture.h"
 #include "event-handlers.h"
-#include "print.h"
-#include "selection.h"
-#include "scranrot.h"
+#include "freezeframe.h"
 #include "state.h"
-#include "state-util.h"
-#include "ui.h"
-#include "util/blend2d.h"
-#include "util/lib-interop.h"
 
 
 static inline void
@@ -79,109 +73,6 @@ handle_image_copy_capture_frame_presentation_time(
 }
 
 
-static inline void
-continue_after_showing_freezeframe(
-    struct scran_output *output
-) {
-    struct scran_output_freezeframe *freezeframe = &output->freezeframe;
-
-    if (freezeframe->unhide_after_capture) {
-        release_selection_surface_hide(output, SCRAN_SELECTION_SURFACE_DISABLE_REASON_FREEZEFRAME_HIDE);
-        freezeframe->unhide_after_capture = false;
-    }
-
-    scran_output_callback callback = freezeframe->callback;
-    assert(callback != NULL);
-    freezeframe->callback = NULL;
-    callback(output);
-}
-
-
-static void display_freezeframe(struct scran_output *output);
-
-static void
-display_freezeframe_after_buffer_release(struct scran_wl_buffer *buffer)
-{
-    struct scran_output *output = &g_state.outputs[get_containing_output_array_index(buffer)];
-    display_freezeframe(output);
-}
-
-static void
-display_freezeframe(struct scran_output *output)
-{
-    struct scran_output_freezeframe *freezeframe = &output->freezeframe;
-
-    struct capture_frame_context    *frame_ctx      = &freezeframe->session.frame_ctx;
-    struct scran_wl_buffer          *capture_buffer = &frame_ctx->scran_wl_buffer;
-    struct scran_wl_buffer          *surface_buffer = &freezeframe->surface_buffer;
-    const struct capture_session_context *session = &freezeframe->session.session_ctx;
-
-    assert(capture_buffer->busy == false); // We should not have started capture if busy
-
-    struct scran_wl_buffer *final_buffer;
-
-    enum wl_output_transform       buffer_transform = -1;
-    const int32_t                  source_width_px  = session->source_dimensions_px.x;
-    const int32_t                  source_height_px = session->source_dimensions_px.y;
-    const enum wl_output_transform source_transform = freezeframe->session.frame_ctx.source_transform;
-
-    // XXX TODO: Rework this once scranrot supports flipped
-    // XXX TODO: Refactor this to make it more readable...
-
-    // Show the new, just-captured freezeframe
-    if (source_transform == WL_OUTPUT_TRANSFORM_NORMAL || source_transform == WL_OUTPUT_TRANSFORM_FLIPPED) {
-        final_buffer     = capture_buffer;
-        buffer_transform = source_transform;
-    } else {
-        bool source_is_flipped = source_transform >= WL_OUTPUT_TRANSFORM_FLIPPED;
-        enum wl_output_transform scranrot_transform = source_is_flipped ? source_transform - WL_OUTPUT_TRANSFORM_FLIPPED : source_transform;
-
-        if (surface_buffer->busy) {
-            surface_buffer->release_callback = display_freezeframe_after_buffer_release;
-            return;
-        }
-
-        assert(get_transformed_width(source_width_px, source_height_px, source_transform) == freezeframe->subsurface.width_px_buffer);
-        assert(get_transformed_height(source_width_px, source_height_px, source_transform) == freezeframe->subsurface.height_px_buffer);
-
-        size_t dst_stride = 0;
-        // See comments referencing #14441 for why we scranrot instead of just ::set_buffer_transform().
-        if (scranrot_transform_framebuffer(
-                capture_buffer->data, source_width_px, source_height_px, source_width_px * session->pixel_stride,
-                surface_buffer->data,
-                RGBA32_SHUFFLE_NO_CHANGE, (enum scranrot_transform)scranrot_transform,
-                &dst_stride)
-        ) {
-            assert(dst_stride < INT_MAX && (int)dst_stride == freezeframe->subsurface.width_px_buffer * session->pixel_stride);
-            final_buffer     = surface_buffer;
-            buffer_transform = source_is_flipped ? WL_OUTPUT_TRANSFORM_FLIPPED : WL_OUTPUT_TRANSFORM_NORMAL;
-        } else {
-            eprintf("WARNING: Scranrot failed to convert freezeframe buffer; falling back to set_buffer_transform.\n");
-            // XXX TODO: This does not work correctly yet without an actual surface.transform
-            // property to check against in the update_scale_size_viewport() functions.
-            final_buffer     = capture_buffer;
-            buffer_transform = source_transform;
-        }
-    }
-    const int final_width_px  = final_buffer == capture_buffer ? source_width_px  : freezeframe->subsurface.width_px_buffer;
-    const int final_height_px = final_buffer == capture_buffer ? source_height_px : freezeframe->subsurface.height_px_buffer;
-
-    final_buffer->busy = true;
-    wl_surface_attach(freezeframe->subsurface.wl_surface, final_buffer->wl_buffer, 0, 0);
-    wl_surface_set_buffer_transform(freezeframe->subsurface.wl_surface, buffer_transform);
-    wl_surface_damage_buffer(freezeframe->subsurface.wl_surface, 0, 0, final_width_px, final_height_px);
-    wl_surface_commit(freezeframe->subsurface.wl_surface);
-    freezeframe->showing = true;
-    {
-        struct scran_ui_context *ui_ctx = &output->selection_surface.ui_ctx;
-        scran_ui_textline_item_set_text( SCRAN_UI_TEXTLINE_VIEW(ui_ctx->ui_keymap), SCRAN_UI_KEYMAP_ITEM_I_FREEZEFRAME, SCRAN_UI_TEXT_KEYMAP_FREEZEFRAME_TURN_OFF);
-        scran_ui_textline_item_set_color(SCRAN_UI_TEXTLINE_VIEW(ui_ctx->ui_keymap), SCRAN_UI_KEYMAP_ITEM_I_FREEZEFRAME, SCRAN_UI_COLOR_KEYMAP_FREEZEFRAME);
-    }
-
-    continue_after_showing_freezeframe(output);
-}
-
-
 static void
 handle_image_copy_capture_frame_ready(
     void *data,
@@ -199,8 +90,8 @@ handle_image_copy_capture_frame_ready(
 
     assert(image_requested || video_requested || freezeframe_requested);
 
-    if (freezeframe_requested) {
-        display_freezeframe(output);
+    if (freezeframe_requested) { // TODO: unlikely()
+        freezeframe_capture_handle_frame_ready(output);
     }
 
     if (image_requested || video_requested) {
@@ -259,9 +150,7 @@ handle_image_copy_capture_frame_failed(
     frame_ctx->frame = NULL;
 
     if (frame_ctx->consumers & SCRAN_CAPTURE_FRAME_CONSUMER_FREEZEFRAME) {
-        eprintf("ERROR: freezeframe capture failed (%u)\n", reason);
-        // FIXME: Handle this better?
-        continue_after_showing_freezeframe(output);
+        freezeframe_capture_handle_failed(output, reason);
     }
     if (frame_ctx->consumers & SCRAN_CAPTURE_FRAME_CONSUMER_IMAGE) {
         capture_image_finish(output);
